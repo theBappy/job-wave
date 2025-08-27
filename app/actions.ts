4433f8bc1d5d1bdbf4b6af65d63c702e1396386d
theBappy <abappy575@gmail.com>
2025-08-27 13:39:42 +0600

    shield, detectBot security layer implemented vai arcject

--- app/actions.ts
+++ app/actions.ts
@@ -5,9 +5,32 @@ import { z } from "zod";
 import { companySchema, jobSeekerSchema } from "./utils/zodSchemas";
 import { prisma } from "./utils/db";
 import { redirect } from "next/navigation";
+import arcjet, { detectBot, shield } from "./utils/arcjet";
+import { request } from "@arcjet/next";
+
+const aj = arcjet
+  .withRule(
+    shield({
+      mode: "LIVE",
+    })
+  )
+  .withRule(
+    detectBot({
+      mode: "LIVE",
+      allow: [],
+    })
+  );
 
 export async function createCompany(data: z.infer<typeof companySchema>) {
   const session = await requireUser();
+
+  const req = await request();
+
+  const decision = await aj.protect(req);
+
+  if (decision.isDenied()) {
+    throw new Error("Forbidden");
+  }
 
   const validateData = companySchema.parse(data);
 
@@ -32,6 +55,14 @@ export async function createCompany(data: z.infer<typeof companySchema>) {
 export async function createJobSeeker(data: z.infer<typeof jobSeekerSchema>) {
   const user = await requireUser();
 
+  const req = await request();
+
+  const decision = await aj.protect(req);
+
+  if (decision.isDenied()) {
+    throw new Error("Forbidden");
+  }
+
   const validateData = jobSeekerSchema.parse(data);
 
   await prisma.user.update({
@@ -49,5 +80,5 @@ export async function createJobSeeker(data: z.infer<typeof jobSeekerSchema>) {
     },
   });
 
-  return redirect("/")
+  return redirect("/");
 }
