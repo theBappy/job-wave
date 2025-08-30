9eb7065555336111a8598985d2b9fae7368b6bdc
theBappy <abappy575@gmail.com>
2025-08-30 14:20:33 +0600

    favorite job save and unsave api implemented

--- components/general/submit-button.tsx
+++ components/general/submit-button.tsx
@@ -2,8 +2,9 @@
 
 import { useFormStatus } from "react-dom";
 import { Button } from "../ui/button";
-import { Loader2 } from "lucide-react";
+import { Heart, Loader2 } from "lucide-react";
 import { ReactNode } from "react";
+import { cn } from "@/lib/utils";
 
 interface Props {
   text: string;
@@ -33,6 +34,31 @@ export function GeneralSubmitButton({ text, variant, width, icon }: Props) {
         <>
           {icon && <div>{icon}</div>}
           <span>{text}</span>
+        </>
+      )}
+    </Button>
+  );
+}
+
+export function SavedJobButton({ savedJob }: { savedJob: boolean }) {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button variant="outline" type="submit" disabled={pending}>
+      {pending ? (
+        <>
+          <Loader2 className="size-4 animate-spin" />
+          <span>Saving...</span>
+        </>
+      ) : (
+        <>
+          <Heart
+            className={cn(
+              savedJob ? "fill-current text-red-500" : "",
+              "size-4 transition-colors"
+            )}
+          />
+          {savedJob ? "Saved" : "Save Job"}
         </>
       )}
     </Button>
