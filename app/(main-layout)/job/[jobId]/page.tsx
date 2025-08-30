24e4125d607aa09fd7350ec07f740c83e31f81a3
theBappy <abappy575@gmail.com>
2025-08-30 13:05:46 +0600

    job details page ui

---
+++ app/(main-layout)/job/[jobId]/page.tsx
@@ -0,0 +1,191 @@
+import { getFlagEmoji } from "@/app/utils/country-list";
+import { prisma } from "@/app/utils/db";
+import { benefits } from "@/app/utils/list-of-benefits";
+import { JsonToHtml } from "@/components/general/json-to-html";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { Heart } from "lucide-react";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+async function getJob(jobId: string) {
+  const jobData = await prisma.jobPost.findUnique({
+    where: {
+      status: "ACTIVE",
+      id: jobId,
+    },
+    select: {
+      jobTitle: true,
+      jobDescription: true,
+      location: true,
+      employmentType: true,
+      benefits: true,
+      createdAt: true,
+      listingDuration: true,
+      Company: {
+        select: {
+          name: true,
+          logo: true,
+          location: true,
+          about: true,
+        },
+      },
+    },
+  });
+
+  if (!jobData) {
+    return notFound();
+  }
+
+  return jobData;
+}
+
+type Params = Promise<{
+  jobId: string;
+}>;
+
+export default async function JobIdPage({ params }: { params: Params }) {
+  const { jobId } = await params;
+  const data = await getJob(jobId);
+  const locationFlag = getFlagEmoji(data.location);
+
+  return (
+    <div className="grid lg:grid-cols-3 gap-8">
+      <div className="space-y-8 col-span-2">
+        {/* header */}
+        <div className="flex items-center justify-between">
+          <div className="">
+            <h1 className="text-2xl font-semibold">{data.jobTitle}</h1>
+            <div className="flex items-center gap-2 mt-2">
+              <p className="font-medium">{data.Company.name}</p>
+              <span className="hidden md:inline text-muted-foreground">*</span>
+              <Badge className="rounded-full" variant="secondary">
+                {data.employmentType}
+              </Badge>
+              <span className="hidden md:inline text-muted-foreground">*</span>
+              <Badge className="rounded-full">
+                {locationFlag && <img src={locationFlag} className="mr-1" />}
+                {data.location}
+              </Badge>
+            </div>
+          </div>
+          <Button variant="outline">
+            <Heart className="size-4" />
+            Save Job
+          </Button>
+        </div>
+
+        <section>
+          <JsonToHtml json={JSON.parse(data.jobDescription)} />
+        </section>
+        <section>
+          <h3 className="font-semibold mb-4">
+            Benefits{" "}
+            <span className="text-sm text-muted-foreground font-normal">
+              (green is offered)
+            </span>
+          </h3>
+          <div className="flex flex-wrap gap-3">
+            {benefits.map((benefit) => {
+              const isOffered = data.benefits.includes(benefit.id);
+              const Icon = benefit.icon;
+              return (
+                <Badge
+                  key={benefit.id}
+                  variant={isOffered ? "default" : "outline"}
+                  className={cn(
+                    isOffered ? "" : "opacity-75 cursor-not-allowed",
+                    "text-sm p-4 py-1.5 rounded-full"
+                  )}
+                >
+                  <Icon className="w-3 h-3" />
+                  <span>{benefit.label}</span>
+                </Badge>
+              );
+            })}
+          </div>
+        </section>
+      </div>
+      <div className="space-y-6">
+        <Card className="p-6">
+          <div className="space-y-4">
+            <div className="">
+              <h3 className="font-semibold">Apply Now</h3>
+              <p className="text-sm text-muted-foreground mt-1">
+                Please let {data.Company.name} know you found this job on
+                JobWave. This helps us grow.{" "}
+              </p>
+            </div>
+            <Button className="w-full">Apply Now</Button>
+          </div>
+        </Card>
+        {/* job details card */}
+        <Card className="p-6">
+          <h3 className="font-semibold">About the Job</h3>
+          <div className="space-y-2">
+            <div className="flex justify-between">
+              <span className="text-sm text-muted-foreground">
+                Apply before
+              </span>
+              <span className="text-sm">
+                {new Date(
+                  data.createdAt.getTime() +
+                    data.listingDuration * 24 * 60 * 60 * 1000
+                ).toLocaleDateString("en-US", {
+                  month: "long",
+                  day: "numeric",
+                  year: "numeric",
+                })}
+              </span>
+            </div>
+            <div className="flex justify-between">
+              <span className="text-sm text-muted-foreground">Posted on</span>
+              <span className="text-sm">
+                {data.createdAt.toLocaleDateString("en-US", {
+                  month: "long",
+                  day: "numeric",
+                  year: "numeric",
+                })}
+              </span>
+            </div>
+            <div className="flex justify-between">
+              <span className="text-sm text-muted-foreground">
+                Employment Type
+              </span>
+              <span className="text-sm">{data.employmentType}</span>
+            </div>
+            <div className="flex justify-between">
+              <span className="text-sm text-muted-foreground">Location</span>
+              <span className="text-sm">
+                {locationFlag && <img src={locationFlag} className="mr-1" />}
+                {data.location}
+              </span>
+            </div>
+          </div>
+        </Card>
+        {/* company info card */}
+        <Card className="p-6">
+          <div className="space-y-4">
+            <div className="flex items-center gap-3">
+              <Image
+                src={data.Company.logo}
+                alt={data.Company.name}
+                width={48}
+                height={48}
+                className="rounded-full size-12"
+              />
+              <div className="flex flex-col">
+                <h3 className="font-semibold">{data.Company.name}</h3>
+                <p className="text-sm line-clamp-3 text-muted-foreground">
+                  {data.Company.about}
+                </p>
+              </div>
+            </div>
+          </div>
+        </Card>
+      </div>
+    </div>
+  );
+}
--- components/general/job-card.tsx
+++ components/general/job-card.tsx
@@ -32,7 +32,7 @@ interface Props {
 
 export function JobCard({ job }: Props) {
   return (
-    <Link href={`/job`}>
+    <Link href={`/job/${job.id}`}>
       <Card className="hover:shadow-lg transition-all duration-300 hover:border-primary">
         <CardHeader>
           <div className="flex flex-col md:flex-row gap-4">
