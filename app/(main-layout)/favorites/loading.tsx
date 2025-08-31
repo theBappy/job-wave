f8c00ef262860fbb3d26c0827fa105e8c8835b22
theBappy <abappy575@gmail.com>
2025-08-31 12:59:00 +0600

    streaming UI with react suspense for loading interface when data is fetching

---
+++ app/(main-layout)/favorites/loading.tsx
@@ -0,0 +1,7 @@
+import { JobListingLoading } from "@/components/streaming-ui/job-listing-loading";
+
+export default function LoadingFavorites(){
+    return (
+        <JobListingLoading />
+    )
+}
